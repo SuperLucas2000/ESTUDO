e9cfc71a4b8b232a4ba26196fcb52e90e1da0e50
Lucas <l.matteus.baptista@gmail.com>
2026-08-15 16:26:07 -0300

    variable 3

--- Python/PythonW3Schools/W3python.py
+++ Python/PythonW3Schools/W3python.py
@@ -42,11 +42,20 @@ print(y)
 """
 
 #CASTING
-
+"""
 #If you want to specify the data type of a variable, this can be done with casting.
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
+"""
+
+#GET THE TYPE
+
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
 
 
 
