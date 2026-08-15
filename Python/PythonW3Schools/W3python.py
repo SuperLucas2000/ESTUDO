a54b5df9ad7cb80549e9f541759311976d7393c1
Lucas <l.matteus.baptista@gmail.com>
2026-08-15 16:19:46 -0300

    variable 1

--- Python/PythonW3Schools/W3python.py
+++ Python/PythonW3Schools/W3python.py
@@ -3,24 +3,26 @@
 x = 5
 y = "Hello, World!" 
 """
-
+#STATEMENT
 """
 #A statement in python usually ends when the line ends.
 #There's no need to use a semicolon.
 print ("Python is fun!")
 """
-
+#STATEMENT2
 """
 #The statements are executed one by one, in the same order as they are written.
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!")
 """
-
+#PRINT TEXT
 """
 # By default, the print() function ends with a new line, to print multiple words on the same line, use the end parameter.
 print("Hello World!", end=" ")
 print("I will print on the same line.")
+"""
+#PRINT NUMBERS
 """
 # To print numbers, just write them inside, without quotes.
 print(3)
@@ -29,4 +31,15 @@ print(50000)
 
 # To mix text and numbers, separate them with a comma.
 print("I am", 24, "year old.")
+"""
+#VARIABLES
+
+#A variable is created the moment you first assign a value to it
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+
 
