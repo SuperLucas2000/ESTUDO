4df06497a1a50d27a8637ea201eefa90af65e789
Lucas <l.matteus.baptista@gmail.com>
2026-09-12 14:25:34 -0300

    comentário

--- Python/PythonBeecrowd/1007.py
+++ Python/PythonBeecrowd/1007.py
@@ -1,8 +1,11 @@
+# Criação das variáveis que receberão os valores.
 A = int(input())
 B = int(input())
 C = int(input())
 D = int(input())
 
+# Variável responsável por guardar o valor do resultado da operação.
 Resultado = ((A * B) - (C * D))
 
+# Printa o valor da variável resultado.
 print(f"DIFERENCA = {Resultado}")
